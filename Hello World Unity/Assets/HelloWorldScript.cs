1939e6a67898629162a2849d95a27f486b6b6819
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2014-03-02 01:38:59 +0000

    Update HelloWorldScript.cs

--- Hello World Unity/Assets/HelloWorldScript.cs
+++ Hello World Unity/Assets/HelloWorldScript.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class HelloWorldScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnGUI(){
 		GUILayout.Label("Hello World");
 	}
